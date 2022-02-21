3e3c11c24263c5319396c8e3ab4d282e9ad5abac
Muvvalakaran <karanmuvvala@yahoo.in>
2022-02-21 11:51:18 -0700

    Added code to Simulate cartpole w CBFs.
    gen_utls.py: Added code to compute the hypercubes for state space defined by the control mat file dumped by rayan
    
    cartpole_utls.py: Added functionality to load the CBF mat file, look up the control polynomial corresponding to the current state space and evolve as per the Controller if applicable.

--- gen_utls.py
+++ gen_utls.py
@@ -1,5 +1,10 @@
+import math
+import copy
 import warnings
 import functools
+
+from typing import Tuple
+
 
 def deprecated(func):
     """This is a decorator which can be used to mark functions
@@ -14,3 +19,64 @@ def deprecated(func):
         warnings.simplefilter('default', DeprecationWarning)  # reset filter
         return func(*args, **kwargs)
     return new_func
+
+
+def create_hypercube_variable_eps(eps, state_space, n) -> Tuple[list, list, list]:
+    """
+        A helper method to create hypercube intervals in each dimension using the variable eps value
+    """
+
+    if eps.shape[0] != state_space.shape[0]:
+        print("Please ensure that the epsilon is specified for each dimension")
+
+    hypercubes_partitions = []
+    hypercubes_partitions_centers = []
+    partition_dimension = []
+
+    for ii in range(n):
+        ith_space = state_space[ii]
+        range_per_interval = 2 * eps[ii]
+        length = max(ith_space) - min(ith_space)
+
+        # For partition of the space the length should be at least 4 epsilon
+        if length < 4 * eps[ii]:
+            print('Error: state space', ii, 'too small for partitioning over chosen epsilon:', eps)
+            print('Try decreasing epsilon [0-1] or normalizing the state space')
+
+        number_of_intervals = math.floor(length / (range_per_interval))
+
+        x_ith_low = min(ith_space)
+        r = range_per_interval
+        m = int(number_of_intervals)
+        partition_dimension.append(m)
+
+        jth_hyper = []
+        jth_hyper_center = []
+        # Assumption: partition floating point errors for domain R negligible
+        for jj in range(int(number_of_intervals)):
+            x_ith_partitions = [x_ith_low + jj * r, x_ith_low + (jj + 1) * r]
+            jth_hyper.append(x_ith_partitions)
+            jth_hyper_center.append(min(jth_hyper[jj]) + eps[ii])
+
+        hypercubes_partitions.append(jth_hyper)
+        hypercubes_partitions_centers.append(jth_hyper_center)
+
+    return hypercubes_partitions, hypercubes_partitions_centers, partition_dimension
+
+
+def recursive_for(hypercube_partitions, dim_count, partition_count, dim, element, hypermatrix):
+
+    for zz in range(len(hypercube_partitions[partition_count])):
+        element1 = copy.deepcopy(element)
+        element1.append(hypercube_partitions[partition_count][zz])
+
+        if partition_count < dim:
+            partition_count += 1
+            recursive_for(hypercube_partitions, dim_count, partition_count, dim, element1, hypermatrix)
+            partition_count -= 1
+        else:
+            element2 = copy.deepcopy(element1)
+            hypermatrix.append(element2)
+            element1.remove(element1[-1])
+
+    return hypermatrix
